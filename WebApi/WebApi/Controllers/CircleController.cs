cf16c9c1029ecd96952ff102a217bf1da7c2c240
tad0910 <165233573+tad0910@users.noreply.github.com>
2025-02-05 14:36:33 +0700

    Add files via upload

---
+++ WebApi/WebApi/Controllers/CircleController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CircleController : ControllerBase
+    {
+        [HttpGet("cv_dt")]
+        public IActionResult tinh_chuvi_dientich_duongkinh(double rr)
+        {
+            if (rr <= 0)
+                return BadRequest("Ban kinh phai duong");
+            double cv = Math.PI * 2 * rr;
+            double dt = Math.PI * rr * rr;
+            double dk = 2 * rr;
+            var json_str = new { dien_tich = dt, chu_vi = cv, duong_kinh = dk };
+            return Ok(json_str);
+        }
+    }
+}
